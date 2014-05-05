24902751757cb6d28f41d765098778147f93ffac
Petr Ječmen <petr.jecmen@tul.cz>
2014-05-05 11:32:49 +0200

    Initial commit. Main class + jMonkey3 library.

---
+++ src/eduapp/EduApp.java
@@ -0,0 +1,31 @@
+package eduapp;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ *
+ * @author Petr Jecmen
+ */
+public class EduApp extends SimpleApplication {
+
+    public static void main(String[] args){
+        EduApp app = new EduApp();
+        app.start(); // start the game
+    }
+ 
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+    
+}
