c003e31108548534d6409aa73a0ba29204462ce5
Petr Ječmen <petr.jecmen@tul.cz>
2014-05-26 12:44:16 +0200

    Slide question input in on activation and slide out on correct answer. closes #7.

--- src/eduapp/gui/GuiQuestInput.java
+++ src/eduapp/gui/GuiQuestInput.java
@@ -1,7 +1,9 @@
 package eduapp.gui;
 
+import de.lessvoid.nifty.EndNotify;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.TextField;
+import de.lessvoid.nifty.effects.EffectEventId;
 import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.input.NiftyInputEvent;
@@ -16,11 +18,13 @@ import eduapp.level.quest.Question;
  */
 public class GuiQuestInput implements ScreenController {
 
+    private static final String NAME_QUESTION_PANEL = "panelQuest";
     private static final String NAME_QUESTION_TEXT = "questText";
     private static final String NAME_QUESTION_INPUT = "questInput";
     private Question question;
     private Element questionText;
     private TextField questionInput;
+    private Element panelQuest;
 
     public void setQuestion(Question question) {
         this.question = question;
@@ -28,6 +32,7 @@ public class GuiQuestInput implements ScreenController {
 
     @Override
     public void bind(Nifty nifty, Screen screen) {
+        panelQuest = nifty.getCurrentScreen().findElementByName(NAME_QUESTION_PANEL);
         questionText = nifty.getCurrentScreen().findElementByName(NAME_QUESTION_TEXT);
         questionInput = nifty.getCurrentScreen().findNiftyControl(NAME_QUESTION_INPUT, TextField.class);
         questionInput.getElement().addInputHandler(new KeyInputHandler() {
@@ -38,9 +43,15 @@ public class GuiQuestInput implements ScreenController {
                         case SubmitText:
                             question.setUserInput(questionInput.getDisplayedText());
                             if (question.isFinished()) {
-                                GuiManager.gotoGameScreen();
+                                panelQuest.startEffect(EffectEventId.onCustom, new EndNotify() {
+                                    @Override
+                                    public void perform() {
+                                        panelQuest.hide();
+                                        GuiManager.gotoGameScreen();
+                                    }
+                                }, "Ok");
                             } else {
-                                questionInput.setText("Wrong !!!");
+                                panelQuest.startEffect(EffectEventId.onCustom, null, "Wrong");
                             }
                             break;
                     }
@@ -54,6 +65,7 @@ public class GuiQuestInput implements ScreenController {
     public void onStartScreen() {
         questionText.getRenderer(TextRenderer.class).setText(question.getQuestion());
         questionInput.setText(question.getUserInput());
+        panelQuest.startEffect(EffectEventId.onCustom, null, "Start");
     }
 
     @Override
