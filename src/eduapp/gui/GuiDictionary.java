0de7e1ce0999151788519e7ebaa2ecd7956a7f59
Petr Ječmen <petr.jecmen@tul.cz>
2014-07-16 08:47:18 +0200

    Open hyperlinks in dictionary on click. Allow multiple links per item. closes #48.

--- src/eduapp/gui/GuiDictionary.java
+++ src/eduapp/gui/GuiDictionary.java
@@ -14,6 +14,10 @@ import eduapp.level.ItemParameters;
 import eduapp.ItemRegistry;
 import eduapp.gui.listbox.Line;
 import eduapp.level.Item;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,7 +30,7 @@ public class GuiDictionary implements ScreenController {
 
     private static final String TAG_DICTIONARY = "dic";
     private static final String SIZE_LINE_HEIGHT = "20";
-    private static final String[] DISPLAY = new String[]{ItemParameters.FORMULA, ItemParameters.STATE, ItemParameters.LINKS};    
+    private static final String[] DISPLAY = new String[]{ItemParameters.FORMULA, ItemParameters.STATE};
     private Nifty nifty;
     private ItemRegistry ir;
     private Element panelContent;
@@ -55,7 +59,7 @@ public class GuiDictionary implements ScreenController {
 //        buildItemTree();
     }
 
-    private void buildItemTree() {       
+    private void buildItemTree() {
         displayedItems.clear();
         tree.clear();
 
@@ -67,9 +71,9 @@ public class GuiDictionary implements ScreenController {
             }
         }
         Collections.sort(displayedItems);
-                        
-        for (Item it : displayedItems) {  
-           tree.addItem(new Line(it.getParam(ItemParameters.NAME)));
+
+        for (Item it : displayedItems) {
+            tree.addItem(new Line(it.getParam(ItemParameters.NAME)));
         }
 
         tree.getElement().layoutElements();
@@ -79,6 +83,7 @@ public class GuiDictionary implements ScreenController {
         for (Element e : panelContent.getElements()) {
             e.markForRemoval();
         }
+        nifty.executeEndOfFrameElementActions();
 
         final Item descr = ir.get(ItemParameters.NAMES_CONVERT);
         if (selectedItem != null) {
@@ -87,6 +92,7 @@ public class GuiDictionary implements ScreenController {
             TextBuilder tb;
             String val;
             Element p;
+            String[] split;
 
             for (String s : DISPLAY) {
                 val = selectedItem.getParam(s);
@@ -95,21 +101,55 @@ public class GuiDictionary implements ScreenController {
                     pb.childLayoutVertical();
                     p = pb.build(nifty, current, panelContent);
 
+                    pb = new PanelBuilder("gap".concat(s));
+                    pb.childLayoutVertical();
+                    pb.height("5%");
+                    pb.build(nifty, current, p);
+
                     tb = new TextBuilder("t1".concat(s));
                     tb.style("baseB");
                     tb.text(descr.getParam(s));
                     tb.color(Color.WHITE);
                     tb.height(SIZE_LINE_HEIGHT);
-                    p.add(tb.build(nifty, current, panelContent));
+                    tb.build(nifty, current, p);
 
                     tb = new TextBuilder("t2".concat(s));
                     tb.style("base");
                     tb.text(val);
                     tb.color(Color.BLACK);
                     tb.height(SIZE_LINE_HEIGHT);
-                    p.add(tb.build(nifty, current, panelContent));
+                    tb.build(nifty, current, p);
+                }
+            }
 
-                    panelContent.add(p);
+            val = selectedItem.getParam(ItemParameters.LINKS);
+            if (val != null) {
+                pb = new PanelBuilder("p".concat(ItemParameters.LINKS));
+                pb.childLayoutVertical();
+                p = pb.build(nifty, current, panelContent);
+
+                pb = new PanelBuilder("gap".concat(ItemParameters.LINKS));
+                pb.childLayoutVertical();
+                pb.height("5%");
+                pb.build(nifty, current, p);
+
+                tb = new TextBuilder("t1".concat(ItemParameters.LINKS));
+                tb.style("baseB");
+                tb.text(descr.getParam(ItemParameters.LINKS));
+                tb.color(Color.WHITE);
+                tb.height(SIZE_LINE_HEIGHT);
+                tb.build(nifty, current, p);
+
+                split = val.split(ItemParameters.SPLITTER);
+                for (String s2 : split) {
+                    tb = new TextBuilder("t2".concat(ItemParameters.LINKS).concat(s2));
+                    tb.style("base");
+                    tb.text(s2);
+                    tb.color("#0000ff");
+                    tb.height(SIZE_LINE_HEIGHT);
+                    tb.visibleToMouse(true);
+                    tb.interactOnClick("hyperlink(" + s2 + ")");
+                    tb.build(nifty, current, p);
                 }
             }
         }
@@ -128,7 +168,14 @@ public class GuiDictionary implements ScreenController {
         }
         buildDataPanel();
     }
-    
+
     public void listBoxItemClicked() {
+    }
+
+    public void hyperlink(String link) throws IOException, URISyntaxException {
+        System.out.println("LINK - " + link);
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(new URI(link));
+        }
     }
 }
