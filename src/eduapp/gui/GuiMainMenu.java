5636c2230e51f7086713ccf5a9a7b2594c598a1b
Petr Ječmen <petr.jecmen@tul.cz>
2014-08-26 09:41:18 +0200

    List of levels is generated from content of directory data\levels.

--- src/eduapp/gui/GuiMainMenu.java
+++ src/eduapp/gui/GuiMainMenu.java
@@ -7,7 +7,8 @@ import de.lessvoid.nifty.controls.ListBoxSelectionChangedEvent;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import eduapp.FlowManager;
-import eduapp.level.LevelList;
+import java.io.File;
+import java.io.FilenameFilter;
 
 /**
  *
@@ -16,13 +17,20 @@ import eduapp.level.LevelList;
 public class GuiMainMenu implements ScreenController {
 
     private static final String LISTBOX_NAME = "LevelList";
-    private ListBox<LevelList> listBox;
+    private static final String LEVEL_EXTENSION = ".xml";
+    private ListBox<String> listBox;
 
     @Override
     public void bind(Nifty nifty, Screen screen) {
         listBox = nifty.getCurrentScreen().findNiftyControl(LISTBOX_NAME, ListBox.class);
-        for (LevelList ln : LevelList.values()) {
-            listBox.addItem(ln);
+        final File f = new File("data\\levels");
+        for (String s : f.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(LEVEL_EXTENSION);
+            }
+        })) {
+            listBox.addItem(s.replace(LEVEL_EXTENSION, ""));
         }
     }
 
@@ -35,12 +43,12 @@ public class GuiMainMenu implements ScreenController {
     }
 
     public void startGame() {
-        FlowManager.getInstance().loadLevel(listBox.getFocusItem().toString());
+        FlowManager.getInstance().loadLevel(listBox.getFocusItem());
     }
 
     @NiftyEventSubscriber(pattern = "LevelList.*")
     public void onListBoxSelectionChanged(final String id,
-            final ListBoxSelectionChangedEvent<LevelList> changed) {
+            final ListBoxSelectionChangedEvent<String> changed) {
         startGame();
     }
 }
