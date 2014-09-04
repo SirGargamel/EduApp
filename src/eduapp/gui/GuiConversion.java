69a8d29dfcda25904fbc50ec6f2de26bf68231d9
Petr Ječmen <petr.jecmen@tul.cz>
2014-09-04 19:19:58 +0200

    Fix font in textfield.

--- src/eduapp/gui/GuiConversion.java
+++ src/eduapp/gui/GuiConversion.java
@@ -1,17 +1,20 @@
 package eduapp.gui;
 
+import com.jme3.niftygui.RenderFontJme;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.builder.TextBuilder;
 import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.controls.textfield.builder.TextFieldBuilder;
 import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.elements.render.TextRenderer;
+import de.lessvoid.nifty.elements.render.TextRenderer.RenderFontNull;
 import de.lessvoid.nifty.input.NiftyInputEvent;
 import de.lessvoid.nifty.screen.KeyInputHandler;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import de.lessvoid.nifty.spi.render.RenderFont;
 import de.lessvoid.nifty.tools.Color;
-import eduapp.FlowManager;
 import eduapp.level.item.ItemParameter;
 import eduapp.level.item.Item;
 import eduapp.level.quest.ConversionQuest;
@@ -22,7 +25,8 @@ import eduapp.level.quest.ConversionQuest;
  */
 public class GuiConversion implements ScreenController {
 
-    private static final String SIZE_LINE_HEIGHT = "20";
+    private static final String SIZE_LINE_HEIGHT = "10%";
+    private static final String SIZE_FULL = "100%";
     private Nifty nifty;
     private ConversionQuest quest;
     private Element panelValues;
@@ -61,47 +65,44 @@ public class GuiConversion implements ScreenController {
         tb.style("baseB");
         tb.text(conversion.getParam(ItemParameter.DESCRIPTION));
         tb.alignCenter();
+        tb.valignCenter();
         tb.color(Color.WHITE);
         e = tb.build(nifty, current, panelValues);
         panelValues.add(e);
 
+        final RenderFont rf = nifty.createFont("interface/Fonts/Base20.fnt");
         for (Item it : quest.getItems()) {
+            pb = new PanelBuilder("p".concat(it.getId()));
+            pb.childLayoutHorizontal();
+            e = pb.build(nifty, current, panelValues);
+
+            pb = new PanelBuilder("pG1".concat(it.getId()));
+            pb.width("1%");
+            pb.build(nifty, current, e);
+            
             tb = new TextBuilder("t".concat(it.getId()));
-            tb.width("10%");
             tb.textHAlignLeft();
             tb.style("base");
             tb.text(it.getParam(from));
             tb.color(Color.WHITE);
-            tb.height(SIZE_LINE_HEIGHT);
+            tb.width("30%");
+            tb.build(nifty, current, e);
+            
+            pb = new PanelBuilder("pG2".concat(it.getId()));
+            pb.width("29%");
+            pb.build(nifty, current, e);
 
             tfb = new TextFieldBuilder("tf".concat(it.getId()));
-            tfb.width("30%");
-            tfb.height(SIZE_LINE_HEIGHT);
             tfb.alignRight();
-
-            pb = new PanelBuilder("p".concat(it.getId()));
-            pb.childLayoutHorizontal();
-            pb.backgroundColor("#8b9dc3");
-            pb.width("100%");
-            e = pb.build(nifty, current, panelValues);
-
-            pb = new PanelBuilder("ps".concat(it.getId()));
-            pb.width("10%");
-            pb.height(SIZE_LINE_HEIGHT);
-
-            e.add(tb.build(nifty, current, e));
-            e.add(pb.build(nifty, current, e));
-
-            text = tfb.build(nifty, current, e);
+            tfb.width("40%");          
+            text = tfb.build(nifty, current, e);            
             text.addInputHandler(new KeyInputHandler() {
                 @Override
                 public boolean keyEvent(NiftyInputEvent nie) {
                     return true;
                 }
             });
-            e.add(text);
-
-            panelValues.add(e);
+            text.getElements().get(0).getElements().get(0).getRenderer(TextRenderer.class).setFont(rf);
         }
 
         panelValues.layoutElements();
