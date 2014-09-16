9468b29e272956ec10588704a1fc6e265afc986c
Petr Ječmen <petr.jecmen@tul.cz>
2014-09-16 12:57:44 +0200

    Display rewards from finished levels in main menu. closes #90.

--- src/eduapp/gui/GuiMainMenu.java
+++ src/eduapp/gui/GuiMainMenu.java
@@ -2,11 +2,15 @@ package eduapp.gui;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyEventSubscriber;
+import de.lessvoid.nifty.builder.ImageBuilder;
+import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.controls.ListBox;
 import de.lessvoid.nifty.controls.ListBoxSelectionChangedEvent;
+import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import eduapp.FlowManager;
+import eduapp.Utils;
 import java.io.File;
 import java.io.FilenameFilter;
 
@@ -18,15 +22,20 @@ public class GuiMainMenu implements ScreenController {
 
     private static final String LISTBOX_NAME = "LevelList";
     private static final String LEVEL_EXTENSION = ".xml";
+    private Nifty nifty;
     private ListBox<String> listBox;
+    private Element rewards;
 
     @Override
     public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
         listBox = nifty.getCurrentScreen().findNiftyControl(LISTBOX_NAME, ListBox.class);
+        rewards = nifty.getCurrentScreen().findElementByName("rewards");
     }
 
     @Override
     public void onStartScreen() {
+        buildRewards();
     }
 
     @Override
@@ -53,6 +62,39 @@ public class GuiMainMenu implements ScreenController {
             counter++;
             if (counter > finishedLevelCount) {
                 break;
+            }
+        }
+
+        buildRewards();
+    }
+
+    private void buildRewards() {
+        if (rewards != null) {
+            for (Element e : rewards.getElements()) {
+                e.markForRemoval();
+            }
+            nifty.executeEndOfFrameElementActions();
+
+            final Screen current = nifty.getCurrentScreen();
+            PanelBuilder pb;
+            ImageBuilder ib;
+            for (int i = 0; i < listBox.itemCount() - 1; i++) {
+                pb = new PanelBuilder("rew" + i);
+                pb.width("10%");
+                pb.backgroundColor("#8b9dc388");
+                pb.childLayoutCenter();
+
+                ib = new ImageBuilder("image" + i);
+                ib.filename(Utils.generateIconFilename(Integer.toString(i + 1)));
+                ib.height("100%");
+                ib.width("100%");
+                pb.image(ib);
+
+                pb.build(nifty, current, rewards);
+
+                pb = new PanelBuilder("gap" + i);
+                pb.width("10%");
+                pb.build(nifty, current, rewards);
             }
         }
     }
